4ea69f27ed830fd5e03842e3a40d63dda55b30b2
adr420 <aditya000000420@gmail.com>
2024-03-18 19:42:17 +0530

    Added drawing functions.

--- js/Draw.js
+++ js/Draw.js
@@ -1,5 +1,33 @@
 
+function Circle(cx,cy,rad)
+{
+	
+}
+Circle.prototype.render = function(){
+	
+}
+
+function Line()
+{
+	
+}
+Line.prototype.render = function(){
+	
+}
+
+function Arc(){
+	
+}
+Arc.prototype.render = function(){
+	
+}
 
 export const Draw  = {
-
+	CTX: null,
+	init()
+	{
+		
+	},
+	Line: Line.bind(Draw),
+	
 };
